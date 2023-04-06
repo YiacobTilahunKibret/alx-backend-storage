76fa3b121c9b4578594feb30fb26dad9d8bd15a8
YiacobTilahunKibret <yiacob.tilahun@mmc-edu.net>
2023-04-07 02:10:01 +0300

    hot fix

--- 0x00-MySQL_Advanced/4-store.sql
+++
@@ -1,4 +0,0 @@
---  creates a trigger that decreases the quantity of an item after adding
---  Quantity in the table items can be negative.
-CREATE TRIGGER decrease_q AFTER INSERT ON orders FOR EACH ROW
-UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
